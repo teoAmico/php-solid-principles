d405e760dda124d0561e64d4d16cee230bbacf3a
Matteo Amico <teoinbox@outlook.com>
2017-04-19 11:09:52 +0200

    added open close principle

--- OpenClosePrinciple/break-example/index.php
+++ OpenClosePrinciple/break-example/index.php
@@ -1,6 +1,6 @@
 <?php
  //open class to extention close to modification
- // this example break principle because if I want to change payment method 
+ // this example break principle because if I want to change  the payment method 
  //I have to change PaypalManager class 
 
  $paypalIpn = new PaypalIpn();
