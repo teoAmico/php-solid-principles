5c5ea499b0af84305e3fbe774bc789e1945ed609
Matteo Amico <teoinbox@outlook.com>
2017-04-19 12:56:26 +0200

    added dependency inversion

---
+++ DependencyInversion/index.php
@@ -0,0 +1,8 @@
+<?php
+
+//high level module shouldn't depend on low level module but both on abstruction
+
+//use dependency injection instead of instanciate an object in a constructor like Logger in Single Responsability principle example
+// or use a Container
+
+
